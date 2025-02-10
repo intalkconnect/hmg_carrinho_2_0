8bd7900e2d21a0080e96d3201d4116eb3173a742
intalkconnect <122463183+intalkconnect@users.noreply.github.com>
2025-02-09 22:38:47 -0300

    Update Summary.js

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
 import {
-  Box,
-  Typography,
-  IconButton,
-  Divider,
-  Grid,
-  Paper,
+    Box,
+    Typography,
+    IconButton,
+    Divider,
+    Grid,
+    Paper,
 } from '@mui/material';
 import { AddCircle, Delete } from '@mui/icons-material';
 import { Pill, FlaskConical, Package, Milk, Archive, FileSearch, SprayCan, Cookie, ShoppingCart } from "lucide-react";
@@ -14,233 +14,237 @@ import { ajustaValor, capitalizeFirstLetter } from '../utils/helpers';
 import Modal from './Modal';
 
 const Summary = ({ orcamentos, updateTotalValue, frete = 0 }) => {
-  const [isModalVisible, setIsModalVisible] = useState(false);
-  const [modalItems, setModalItems] = useState([]);
+    const [isModalVisible, setIsModalVisible] = useState(false);
+    const [modalItems, setModalItems] = useState([]);
 
-  const handleIncrement = (orcamento) => {
-    orcamento.orc_qt_potes++;
-    updateTotalValue(orcamentos);
-  };
+    const handleIncrement = (orcamento) => {
+        orcamento.orc_qt_potes++;
+        updateTotalValue(orcamentos);
+    };
 
-  const handleDecrement = (orcamento) => {
-    if (orcamento.orc_qt_potes > 0) {
-      orcamento.orc_qt_potes--;
-      updateTotalValue(orcamentos);
-    }
-  };
+    const handleDecrement = (orcamento) => {
+        if (orcamento.orc_qt_potes > 0) {
+            orcamento.orc_qt_potes--;
+            updateTotalValue(orcamentos);
+        }
+    };
 
-  const handleOpenModal = (items) => {
-    setModalItems(items);
-    setIsModalVisible(true);
-    document.activeElement.blur();
-  };
+    const handleOpenModal = (items) => {
+        setModalItems(items);
+        setIsModalVisible(true);
+        document.activeElement.blur();
+    };
 
-  const handleCloseModal = () => {
-    setIsModalVisible(false);
-    document.activeElement.blur();
-  };
+    const handleCloseModal = () => {
+        setIsModalVisible(false);
+        document.activeElement.blur();
+    };
 
-const getIconByType = (tipo) => {
-  // Função para normalizar string (remover acentos e transformar em maiúsculas)
-  const normalizeText = (text) => 
-    text?.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toUpperCase();
+    const getIconByType = (tipo) => {
+        // Função para normalizar string (remover acentos e transformar em maiúsculas)
+        const normalizeText = (text) =>
+            text?.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toUpperCase();
 
-  const normalizedType = normalizeText(tipo || '');
+        const normalizedType = normalizeText(tipo || '');
 
-  if (normalizedType === "CAPSULA") return <Pill color="#00796b" />;
-  if (["CREME", "LOCAO", "XAMPU", "GEL", "POMADA"].includes(normalizedType)) {
-    return <Milk color="secondary" />;
-  }
-  if (["XAROPE", "SOLUCAO ORAL"].includes(normalizedType)) {
-    return <FlaskConical color="#673ab7" />;
-  }
-  if (normalizedType === "FILTRO SOLAR") return <SprayCan color="#9e9e9e" />;
-  if (normalizedType === "BISCOITO MEDICAMENTOSO") return <Cookie color="#9e9e9e" />;
-  if (normalizedType === "ENVELOPE") return <Package color="#ff9800" />;
-  
-  return <Archive color="disabled" />;
-};
+        if (normalizedType === "CAPSULA") return <Pill color="#00796b" />;
+        if (["CREME", "LOCAO", "XAMPU", "GEL", "POMADA"].includes(normalizedType)) {
+            return <Milk color="secondary" />;
+        }
+        if (["XAROPE", "SOLUCAO ORAL"].includes(normalizedType)) {
+            return <FlaskConical color="#673ab7" />;
+        }
+        if (normalizedType === "FILTRO SOLAR") return <SprayCan color="#9e9e9e" />;
+        if (normalizedType === "BISCOITO MEDICAMENTOSO") return <Cookie color="#9e9e9e" />;
+        if (normalizedType === "ENVELOPE") return <Package color="#ff9800" />;
 
-  const totalValue =
-    orcamentos.reduce(
-      (sum, item) =>
-        sum + parseFloat((item.orc_valor_liquido * item.orc_qt_potes).toFixed(2)),
-      0
-    ) + parseFloat(frete);
+        return <Archive color="disabled" />;
+    };
 
-  return (
-    <>
-      <Paper
-        id="summary"
-        elevation={3}
-        sx={{
-          padding: 3,
-          borderRadius: 2,
-          backgroundColor: '#ffffff',
-          color: '#333333',
-        }}
-      >
-                                    <Box 
-                sx={{ 
-                    display: 'flex', 
-                    alignItems: 'center',
-                    gap: 1,
-                    color: '#00BFBE'
+    const totalValue =
+        orcamentos.reduce(
+            (sum, item) =>
+                sum + parseFloat((item.orc_valor_liquido * item.orc_qt_potes).toFixed(2)),
+            0
+        ) + parseFloat(frete);
+
+    return (
+        <>
+            <Paper
+                id="summary"
+                elevation={3}
+                sx={{
+                    padding: 3,
+                    borderRadius: 2,
+                    backgroundColor: '#ffffff',
+                    color: '#333333',
                 }}
             >
-                <ShoppingCart />
-                <Typography 
-                    variant="body2" 
-                        sx={{ 
-        color: '#1E293B',
-        fontWeight: 600,
-        lineHeight: 1.2
-    }}
-                >
-                    Seu Pedido
-                </Typography>
-            </Box>
-        <Divider sx={{ mb: 2, borderColor: '#e0e0e0' }} />
-        <Box>
-          {orcamentos.map((item, index) => (
-            <Grid
-              container
-              key={index}
-              spacing={1}
-              sx={{
-                alignItems: 'center',
-                mb: 2,
-                pb: 2,
-                borderBottom: '1px solid #e0e0e0',
-              }}
-            >
-              {/* Ícone e Nome do Produto */}
-              <Grid item xs={1}>
-                {getIconByType(item.orc_forma_farmac)}
-              </Grid>
-              <Grid item xs={8}>
+                                    // Substitua o Box do título por:
                 <Box
-                  sx={{
-                    display: 'flex',
-                    justifyContent: 'space-between',
-                    alignItems: 'center',
-                  }}
+                    sx={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        gap: 1,
+                        mb: 2  // Adicionando margem bottom
+                    }}
                 >
-                  <Typography
-                    variant="body1"
-                    fontWeight="bold"
-                    noWrap
-                    sx={{ color: '#333333', fontSize: '0.9rem' }}
-                  >
-                    {capitalizeFirstLetter(
-                      item.orcamentoItens[0]?.orc_Produto_Nome.trim() ||
-                      'Produto desconhecido'
-                    )}
+                    <ShoppingCart
+                        size={24}
+                        color="#00BFBE"
+                    />
                     <Typography
-                      component="span"
-                      sx={{ fontSize: '0.85rem', color: '#666666', ml: 1 }}
+                        variant="h6"
+                        sx={{
+                            color: '#00BFBE',
+                            fontWeight: 600,
+                            fontSize: '1.1rem'
+                        }}
                     >
-                      ({item.orcamentoItens[0]?.orc_Produto_quantidade || 0}{' '}
-                      {item.orcamentoItens[0]?.orc_Produto_unidade.trim() || ''})
+                        Seu Pedido
                     </Typography>
-                  </Typography>
-                  {item.orcamentoItens.length > 1 && (
-                    <IconButton
-                      size="small"
-                      sx={{ color: '#00695c' }}
-                      onClick={() => handleOpenModal(item.orcamentoItens)}
-                    >
-                      <FileSearch />
-                    </IconButton>
-                  )}
                 </Box>
-                {item.orcamentoItens.length > 1 && (
-                  <Typography variant="body2" sx={{ color: '#666666' }}>
-                    e demais componentes
-                  </Typography>
-                )}
-                <Typography variant="body2" sx={{ color: '#666666', mt: 0.5 }}>
-                  {item.orc_volume} {item.orc_Volume_Unidade}
-                </Typography>
-              </Grid>
+                <Divider sx={{ mb: 2, borderColor: '#e0e0e0' }} />
+                <Box>
+                    {orcamentos.map((item, index) => (
+                        <Grid
+                            container
+                            key={index}
+                            spacing={1}
+                            sx={{
+                                alignItems: 'center',
+                                mb: 2,
+                                pb: 2,
+                                borderBottom: '1px solid #e0e0e0',
+                            }}
+                        >
+                            {/* Ícone e Nome do Produto */}
+                            <Grid item xs={1}>
+                                {getIconByType(item.orc_forma_farmac)}
+                            </Grid>
+                            <Grid item xs={8}>
+                                <Box
+                                    sx={{
+                                        display: 'flex',
+                                        justifyContent: 'space-between',
+                                        alignItems: 'center',
+                                    }}
+                                >
+                                    <Typography
+                                        variant="body1"
+                                        fontWeight="bold"
+                                        noWrap
+                                        sx={{ color: '#333333', fontSize: '0.9rem' }}
+                                    >
+                                        {capitalizeFirstLetter(
+                                            item.orcamentoItens[0]?.orc_Produto_Nome.trim() ||
+                                            'Produto desconhecido'
+                                        )}
+                                        <Typography
+                                            component="span"
+                                            sx={{ fontSize: '0.85rem', color: '#666666', ml: 1 }}
+                                        >
+                                            ({item.orcamentoItens[0]?.orc_Produto_quantidade || 0}{' '}
+                                            {item.orcamentoItens[0]?.orc_Produto_unidade.trim() || ''})
+                                        </Typography>
+                                    </Typography>
+                                    {item.orcamentoItens.length > 1 && (
+                                        <IconButton
+                                            size="small"
+                                            sx={{ color: '#00695c' }}
+                                            onClick={() => handleOpenModal(item.orcamentoItens)}
+                                        >
+                                            <FileSearch />
+                                        </IconButton>
+                                    )}
+                                </Box>
+                                {item.orcamentoItens.length > 1 && (
+                                    <Typography variant="body2" sx={{ color: '#666666' }}>
+                                        e demais componentes
+                                    </Typography>
+                                )}
+                                <Typography variant="body2" sx={{ color: '#666666', mt: 0.5 }}>
+                                    {item.orc_volume} {item.orc_Volume_Unidade}
+                                </Typography>
+                            </Grid>
 
-              {/* Controle de Quantidade */}
-              <Grid item xs={6}>
-                <Box
-                  sx={{
-                    display: 'flex',
-                    alignItems: 'center',
-                    justifyContent: 'flex-start',
-                    gap: 1,
-                  }}
-                >
-                  <IconButton
-                    size="small"
-                    sx={{ color: '#00695c' }}
-                    onClick={() => handleDecrement(item)}
-                  >
-                    <Delete />
-                  </IconButton>
-                  <Typography sx={{ color: '#333333' }}>
-                    {item.orc_qt_potes}
-                  </Typography>
-                  <IconButton
-                    size="small"
-                    sx={{ color: '#00695c' }}
-                    onClick={() => handleIncrement(item)}
-                  >
-                    <AddCircle />
-                  </IconButton>
+                            {/* Controle de Quantidade */}
+                            <Grid item xs={6}>
+                                <Box
+                                    sx={{
+                                        display: 'flex',
+                                        alignItems: 'center',
+                                        justifyContent: 'flex-start',
+                                        gap: 1,
+                                    }}
+                                >
+                                    <IconButton
+                                        size="small"
+                                        sx={{ color: '#00695c' }}
+                                        onClick={() => handleDecrement(item)}
+                                    >
+                                        <Delete />
+                                    </IconButton>
+                                    <Typography sx={{ color: '#333333' }}>
+                                        {item.orc_qt_potes}
+                                    </Typography>
+                                    <IconButton
+                                        size="small"
+                                        sx={{ color: '#00695c' }}
+                                        onClick={() => handleIncrement(item)}
+                                    >
+                                        <AddCircle />
+                                    </IconButton>
+                                </Box>
+                            </Grid>
+
+                            {/* Preço */}
+                            <Grid item xs={6} textAlign="right">
+                                <Typography
+                                    variant="body1"
+                                    fontWeight="bold"
+                                    sx={{ color: '#004D40' }}
+                                >
+                                    R$ {ajustaValor(item.orc_valor_liquido * item.orc_qt_potes)}
+                                </Typography>
+                            </Grid>
+                        </Grid>
+                    ))}
                 </Box>
-              </Grid>
+                <Box sx={{ textAlign: 'right', mt: 2 }}>
+                    {/* Exibição do Frete */}
+                    <Box sx={{ display: 'flex', justifyContent: 'space-between', mb: 1 }}>
+                        <Typography variant="body2" color="#666666">
+                            Frete:
+                        </Typography>
+                        <Typography variant="body1" fontWeight="bold" sx={{ color: '#00695c' }}>
+                            R$ {ajustaValor(parseFloat(frete))}
+                        </Typography>
+                    </Box>
 
-              {/* Preço */}
-              <Grid item xs={6} textAlign="right">
-                <Typography
-                  variant="body1"
-                  fontWeight="bold"
-                  sx={{ color: '#004D40' }}
-                >
-                  R$ {ajustaValor(item.orc_valor_liquido * item.orc_qt_potes)}
-                </Typography>
-              </Grid>
-            </Grid>
-          ))}
-        </Box>
-        <Box sx={{ textAlign: 'right', mt: 2 }}>
-          {/* Exibição do Frete */}
-          <Box sx={{ display: 'flex', justifyContent: 'space-between', mb: 1 }}>
-            <Typography variant="body2" color="#666666">
-              Frete:
-            </Typography>
-            <Typography variant="body1" fontWeight="bold" sx={{ color: '#00695c' }}>
-              R$ {ajustaValor(parseFloat(frete))}
-            </Typography>
-          </Box>
+                    {/* Exibição do Total */}
+                    <Box sx={{ display: 'flex', justifyContent: 'space-between', mb: 1 }}>
+                        <Typography variant="body2" color="#666666">
+                            Total:
+                        </Typography>
+                        <Typography variant="h5" fontWeight="bold" sx={{ color: '#00695c' }}>
+                            R$ {ajustaValor(parseFloat(totalValue))}
+                        </Typography>
+                    </Box>
 
-          {/* Exibição do Total */}
-          <Box sx={{ display: 'flex', justifyContent: 'space-between', mb: 1 }}>
-            <Typography variant="body2" color="#666666">
-              Total:
-            </Typography>
-            <Typography variant="h5" fontWeight="bold" sx={{ color: '#00695c' }}>
-              R$ {ajustaValor(parseFloat(totalValue))}
-            </Typography>
-          </Box>
+                    {/* Exibição do Parcelamento */}
+                    <Typography variant="body2" color="#666666">
+                        {(parseFloat(totalValue) + parseFloat(frete)) > 0
+                            ? `ou 4x de R$ ${ajustaValor(parseFloat(totalValue) / 4)} sem juros`
+                            : 'Adicione itens ao carrinho para calcular o parcelamento'}
+                    </Typography>
+                </Box>
+            </Paper>
 
-          {/* Exibição do Parcelamento */}
-          <Typography variant="body2" color="#666666">
-            {(parseFloat(totalValue) + parseFloat(frete)) > 0
-              ? `ou 4x de R$ ${ajustaValor(parseFloat(totalValue) / 4)} sem juros`
-              : 'Adicione itens ao carrinho para calcular o parcelamento'}
-          </Typography>
-        </Box>
-      </Paper>
-
-      {/* Modal */}
-      <Modal isVisible={isModalVisible} onClose={handleCloseModal} items={modalItems} />
-    </>
-  );
+            {/* Modal */}
+            <Modal isVisible={isModalVisible} onClose={handleCloseModal} items={modalItems} />
+        </>
+    );
 };
 
 export default Summary;
